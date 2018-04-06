cb6f14d12d5f76ef2ece8453e8cdfccce23d2d46
t3rmin41 <t33rmin41@gmail.com>
2018-04-06 15:26:10 +0300

    Console Springboot

--- src/main/java/com/simple/geocoding/app/GeocodingApp.java
+++ src/main/java/com/simple/geocoding/app/GeocodingApp.java
@@ -2,12 +2,24 @@ package com.simple.geocoding.app;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class GeocodingApp {
+@SpringBootApplication
+public class GeocodingApp implements CommandLineRunner {
   
   private static final Logger logger = LoggerFactory.getLogger(GeocodingApp.class);
   
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+    SpringApplication app = new SpringApplication(GeocodingApp.class);
+    app.setBannerMode(Banner.Mode.OFF);
+    app.run(args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
     logger.warn("Hello World!");
   }
 }
