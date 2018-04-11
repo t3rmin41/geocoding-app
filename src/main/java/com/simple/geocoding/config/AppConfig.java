00651a5acbb1f6089b0501edb6df486abe265341
t3rmin41 <t33rmin41@gmail.com>
2018-04-11 11:10:31 +0300

    Database added

--- src/main/java/com/simple/geocoding/config/AppConfig.java
+++ src/main/java/com/simple/geocoding/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.simple.geocoding.config;
 
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,4 +10,11 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = {"com.simple.geocoding.service", "com.simple.geocoding.mapper", "com.simple.geocoding.jms", "com.simple.geocoding.util"})
 public class AppConfig {
 
+  @Bean
+  public ServletRegistrationBean h2servletRegistration(){
+      ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+      registrationBean.addUrlMappings("/h2-console/*");
+      return registrationBean;
+  }
+  
 }
